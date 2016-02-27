d803e92bdc1271a8ca5f6b54b035d750e672ef8a
crystalwitkowski <crystalgenevieve@gmail.com>
2016-02-26 18:35:02 -0800

    Added files via upload

---
+++ HenryBooksCH6pt2.sql
@@ -0,0 +1,7 @@
+-- QUESTION#3
+--SET PRICE = PRICE * .04; I'm having trouble with this question. Still formulating.
+
+--Question#4 
+INSERT INTO FICTION
+VALUES
+('9946','Cannery Row','PE','11.95');

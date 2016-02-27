b4596f4ed10881b0c6ef2057155a4426c96dce15
crystalwitkowski <crystalgenevieve@gmail.com>
2016-02-26 18:34:45 -0800

    Delete HenryBooksCH6pt2.sql

--- HenryBooksCH6pt2.sql
+++
@@ -1,7 +0,0 @@
--- QUESTION#3
---SET PRICE = PRICE * .04; I'm having trouble with this question. Still formulating.
-
---Question#4 
-INSERT INTO FICTION
-VALUES
-('9946','Cannery Row','PE','11.95');
